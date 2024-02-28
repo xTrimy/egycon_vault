5443856ec7ef97a9a225860d4afc835641ededad
NRDAIZL <nadine2110371@miuegypt.edu.eg>
2024-02-28 14:08:40 +0200

    Revert "Revert "history view +migration""
    This reverts commit 4665699e3597e7cc9592d17658bdaf62229d6c1c.

--- resources/views/history.blade.php
+++ resources/views/history.blade.php
@@ -1,1 +1,24 @@
+@extends('layouts.app')
+@section('page')
+  history
+@endsection
+@section('title')
+  Actions History
+@endsection
+@section('content')
+  <main class="h-full pb-16 overflow-y-auto">
+  <div class="container grid px-6 mx-auto">
+        <h2 class="my-6 text-2xl font-semibold text-gray-700 dark:text-gray-200">
+          History
+        </h2>
 
+        <div class="w-full overflow-hidden rounded-lg shadow-xs">
+          <div class="w-full overflow-x-auto">
+          </div>
+          <div class="mt-4">
+            <!-- Pagination -->
+          </div>
+        </div>
+      </div>
+  </main>
+@endsection
